5bdd88bb8a5f502c2e1d87298e40a360d13b9d69
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2022-10-28 14:12:52 +0200

    Nuevos ficheros de prueba

---
+++ ArchivoJAVA.java
@@ -0,0 +1,15 @@
+
+public class TiradaAleatoria {
+
+  public static void main(String[] args) {
+    
+    
+    int tiradaDado = (int)(Math.random()*6+1);
+    int mesAleatorio = (int)(Math.random()*12+1);
+    
+    System.out.println(tiradaDado);
+    System.out.println(mesAleatorio);
+
+  }
+
+}
